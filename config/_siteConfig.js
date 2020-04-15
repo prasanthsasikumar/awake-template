45b911283ad39f0a5b33da916aef19bf4d7af8d2
Prasanth Sasikumar <prasanths99@yahoo.co.in>
2020-04-16 01:45:45 +1200

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -69,7 +69,7 @@ export default {
     mailchimp: {
       on: true,
       formAction:
-        'https://danielkelly.us3.list-manage.com/subscribe/post?u=d3c2e762b66a2353d0bc29bf3&amp;id=2abeaa06e0'
+        'https://prasanthsasikumar.us4.list-manage.com/subscribe/post?u=2e57f331790dff4d1ec177b1b&amp;id=43a5087d54'
     },
     custom: {
       on: false,
