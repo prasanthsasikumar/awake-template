4961d4fb45633dba0a2a32a0914d6e46d160c8d6
Prasanth Sasikumar <psas598@UoA.auckland.ac.nz>
2020-04-16 20:28:27 +1200

    contact form

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,62 +1,75 @@
 <template>
-  <div id="contact-page" class="page-wrapper contact-page content-page">
-    <site-hero
-      title="Contact Me"
-      subtitle="Drop me a line."
-      image="/uploads/contact-hero.jpg"
-    ></site-hero>
-    <main-section theme="sidebar-right">
-      <template v-slot:default>
-        <div class="tile is-ancestor">
-          <div class="tile is-parent">
-            <article class="tile is-child box">
-              <p class="title">
-                Hello!
-              </p>
-              <p class="subtitle">
-                What can I do for you?
-              </p>
-              <figure class="image is-1by1 ">
-                <opti-image
-                  :src="require('~/assets/uploads/contact-person.jpg').src"
-                  :srcset="
+<div id="contact-page" class="page-wrapper contact-page content-page">
+  <site-hero title="Contact Me" subtitle="Drop me a line." image="/uploads/contact-hero.jpg"></site-hero>
+  <main-section theme="sidebar-right">
+    <template v-slot:default>
+      <div class="tile is-ancestor">
+        <div class="tile is-parent">
+          <article class="tile is-child box">
+            <p class="title">
+              Hello!
+            </p>
+            <p class="subtitle">
+              What can I do for you?
+            </p>
+            <figure class="image is-1by1 ">
+              <opti-image :src="require('~/assets/uploads/contact-person.jpg').src" :srcset="
                     require('~/assets/uploads/contact-person.jpg').srcSet
-                  "
-                />
-              </figure>
-              <br />
-              <div class="content">
-                <p>
-                  <strong>
-                    Edit this page in<code>/pages/contact.vue</code>to fit your
-                    needs.
-                  </strong>
-                </p>
-                <p>
-                  This form doesn't actually work but would be easy enough to
-                  slap an action on to have it post wherever you'd like. Or put
-                  the netlify attribute on it and have netlify handle all your
-                  form submissions...
-                </p>
-              </div>
-            </article>
-          </div>
-          <div class="tile is-parent is-8">
-            <article class="tile is-child box">
-              <contact-form />
-            </article>
-          </div>
+                  " />
+            </figure>
+            <br />
+            <div class="content">
+              <p>
+                <strong>
+                  Edit this page in<code>/pages/contact.vue</code>to fit your
+                  needs.
+                </strong>
+              </p>
+              <p>
+                This form doesn't actually work but would be easy enough to
+                slap an action on to have it post wherever you'd like. Or put
+                the netlify attribute on it and have netlify handle all your
+                form submissions...
+              </p>
+            </div>
+          </article>
         </div>
-      </template>
-      <template v-slot:sidebar>
-        <h3 class="subtitle is-4">
-          Latest Posts
-        </h3>
-        <!-- Latest Posts -->
-        <posts-grid :per-row="1" :number="2" />
-      </template>
-    </main-section>
-  </div>
+        <div class="tile is-parent is-8">
+          <article class="tile is-child box">
+            <form name="contact" method="POST" data-netlify="true">
+              <p>
+                <label>Your Name: <input type="text" name="name" /></label>
+              </p>
+              <p>
+                <label>Your Email: <input type="email" name="email" /></label>
+              </p>
+              <p>
+                <label>Your Role: <select name="role[]" multiple>
+                    <option value="leader">Leader</option>
+                    <option value="follower">Follower</option>
+                  </select></label>
+              </p>
+              <p>
+                <label>Message: <textarea name="message"></textarea></label>
+              </p>
+              <p>
+                <button type="submit">Send</button>
+              </p>
+            </form>
+            <contact-form />
+          </article>
+        </div>
+      </div>
+    </template>
+    <template v-slot:sidebar>
+      <h3 class="subtitle is-4">
+        Latest Posts
+      </h3>
+      <!-- Latest Posts -->
+      <posts-grid :per-row="1" :number="2" />
+    </template>
+  </main-section>
+</div>
 </template>
 <script>
 import ContactForm from '~/components/ContactForm'
@@ -66,6 +79,8 @@ export default {
       title: `Contact | ${this.$siteConfig.siteName}`
     }
   },
-  components: { ContactForm }
+  components: {
+    ContactForm
+  }
 }
 </script>
